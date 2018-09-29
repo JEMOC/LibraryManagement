f10998c9f13fbe09a1c8719c19b243aa899a20b1
JEMOC <ccccjiemo@gmail.com>
2018-09-29 22:54:29 +0800

    input

--- src/components/router/InputManagement.vue
+++ src/components/router/InputManagement.vue
@@ -1,166 +1,232 @@
 <template>
-    <div class="input">
-        <div class="basebox clearfix">
-            <div class="coverbox">
-                <label for="up" class="cover-mask" :class="{add: !info.cover}"></label>
-                <input id="up" style="display:none" type="file" @change="getImage($event)">
-                <img :src="info.cover" alt="" class="cover">
-            </div>
-            <div class="info">
-                <div class="baseinfo">
-                    <div style="width: 80%; border-bottom: 1px solid #a9a9a9" class="clearfix">
-                        <input type="text" placeholder="书名" v-model="info.name">
-                        <input type="text" placeholder="副标题" v-model="info.subHead">
-                        <input type="text" placeholder="原作名" v-model="info.original">
-                        <input type="text" placeholder="ISBN" v-model="info.isbn">
-                        <input type="text" placeholder="作者" v-model="info.author">
-                        <input type="text" placeholder="出版社" v-model="info.publish">
-                        <input type="date" v-model="info.date">
-                    </div>
-                </div>
-                <div class="synopsis">
-                    <div class="biography">
-                        <label for="p2">个人简介</label>
-                        <textarea id="p2" style="resize: none; height: 160px; width: 80%; font-size: 14px" v-model="info.synopsis.author"></textarea>
-                    </div>
-                    <div class="content-validity">
-                        <label for="p3">内容简介</label>
-                        <textarea name="" id="p3" style="resize: none; height: 160px; width: 80%; font-size: 14px"
-                            v-model="info.synopsis.content"></textarea>
-                    </div>
-                    <button @click="sumbit()">提交</button>
-                </div>
-
-            </div>
-
+  <div class="input">
+    <div class="container">
+      <div class="info clearfix">
+        <div class="cover">
+          <label for="file"></label>
+          <a class="noimage">点击添加图片</a>
+          <input type="file" name="file" id="file" accept="image/*" style="display: none;" @change="uploadImg($event)">
+          <img :src="info.cover" alt="" class="cover_img">
         </div>
+        <div class="baseinfo">
+          <div class="text-box">
+            <input type="text" id="name" v-model="info.name">
+            <label for="name" :class="{'rise': info.name}">书名</label>
+          </div>
+          <div class="text-box">
+            <input type="text" id="subhead" v-model="info.subHead">
+            <label for="subhead" :class="{'rise': info.subHead}">副标题</label>
+          </div>
+          <div class="text-box">
+            <input type="text" id="isbn" v-model="info.isbn">
+            <label for="isbn" :class="{'rise': info.isbn}">ISBN</label>
+          </div>
+          <div class="text-box">
+            <input type="text" id="author" v-model="info.author">
+            <label for="author" :class="{'rise': info.author}">作者</label>
+          </div>
+          <div class="text-box">
+            <input type="text" id="publish" v-model="info.publish">
+            <label for="publish" :class="{'rise': info.publish}">出版社</label>  
+          </div>
+          <div class="text-box">
+            <input type="date" v-model="info.date" id="date">
+          </div>
+        </div>
+      </div>
+      <hr>
+      <div class="synopsis">
+        <div class="author-box">
+          <p>作者简介</p>
+          <textarea v-model="info.synopsis.author"></textarea>
+        </div>
+        <div class="content-box">
+          <p>内容简介</p>
+          <textarea v-model="info.synopsis.content"></textarea>
+        </div>
+      </div>
     </div>
+
+  </div>
 </template>
 
-<script>
-    export default {
-        name: 'Input',
-        data() {
-            return {
-                url: '',
-                info: {
-                    name: '',
-                    cover: '',
-                    subHead: '',
-                    original: '',
-                    isbn: '',
-                    author: '',
-                    publish: '',
-                    date: '',
-                    synopsis: {
-                        author: '',
-                        content: ''
-                    }
-                }
-            }
-        },
-        methods: {
-            getImage: function (e) {
-                let that = this
-                let reader = new FileReader()
-                reader.readAsDataURL(e.target.files[0])
-                reader.onloadend = function () {
-                    let URL = reader.result;
-                    that.info.cover = URL
-                }
-
-            },
-            print: function(){
-                console.log(this.info)
-            },
-            sumbit: function(){
-                let that = this
-                that.$http
-                    .get('http://123')
-                    .then((data) => {
-                        return data
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                        console.log('未设定交互url')
-                    })
-            }
-        },
-        computed: {
-
-        }
-    }
-</script>
-
 <style lang="scss">
-    .input {
-        .basebox {
-            .coverbox {
-                width: 160px;
-                height: 220px;
-                margin-left: 20px;
-                position: relative;
-                float: left;
-                overflow: hidden;
 
-                .cover-mask {
-                    display: block;
-                    position: absolute;
-                    height: 216px;
-                    width: 156px;
-                    cursor: pointer;
-                    background-color: transparent;
-                }
+.input {
+  .container {
+    width: 800px;
+    margin: auto;
 
-                .add {
-                    border: 2px dashed;
-                    color: #ccc;
-                }
-
-                .cover {
-                    width: 100%;
-                    height: 100;
-                }
-            }
-
-            .info {
-                display: block;
-                overflow: hidden;
-                padding: 0 40px 0 80px;
-
-
-
-                .baseinfo {
-                    margin-bottom: 10px;
-
-
-                    input {
-                        width: 200px;
-                        font-size: 16px;
-                        line-height: 20px;
-                        text-indent: 5px;
-                        padding: 5px 10px;
-                        margin: 0 40px 20px 0;
-                        border: 0;
-                        border-bottom: 1px solid #ccc;
-                        outline: none;
-
-
-                    }
-                }
-
-                .synopsis {
-                    div {
-                        margin-bottom: 30px;
-                    }
-
-                    label {
-                        display: block;
-                        padding-bottom: 8px;
-                    }
-                }
-            }
-
-        }
+    input{
+      -webkit-appearance: none;
+      outline: none;
     }
+
+    .cover {
+      position: relative;
+      float: right;
+      width: 160px;
+      height: 212px;
+      margin-right: 15px;
+
+      a{
+        display: none;
+      }
+
+      .cover_img{
+        display: block;
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        left: 0;
+      }      
+
+      .noimage{
+        display: block;
+        width: 156px;
+        height: 208px;
+        border: 2px dashed #ccc;
+        line-height: 210px;
+        text-align: center;
+        color: #ccc;
+      }
+
+      label {
+        position: absolute;
+        display: block;
+        height: 100%;
+        width: 100%;
+        cursor: pointer;
+        z-index: 99;
+      }
+    }
+
+    .baseinfo {
+
+
+      .text-box{
+        position: relative;
+        display: inline-block;
+        margin-bottom: 40px;
+        margin-right: 15px;
+
+        input{
+          border: solid #798695;
+          border-width: 2px 0;
+          width: 280px;
+          line-height: 30px;
+          background: transparent;
+          font-size: 14px;
+        }
+
+        label{
+          position: absolute;
+          left: 0;
+          line-height: 36px;
+          cursor: text;
+          top: 0;
+          color: #798695;
+          font-size: 14px;
+          transition: all .1s linear;
+        }
+
+        input:focus + label {
+          line-height: 20px;
+          top: -22px;
+        }
+
+        .rise{
+          line-height: 20px;
+          top: -22px;
+        }
+      }
+    }
+
+    .synopsis{
+      margin-right: 15px;
+      margin-top: 20px;
+
+      p{
+        padding: 0 0 .5em 0;
+        font-size: 15px;
+      };
+
+      .author-box, .content-box{
+        margin-bottom: 15px;
+      }
+
+      textarea{
+        resize: none;
+        width: 100%;
+        height: 160px;
+        outline: none;
+        font-size: 16px;
+        text-indent: .8em;
+      }
+    }
+  }
+}
 </style>
+
+<script>
+export default {
+  name: "Input",
+  data() {
+    return {
+      url: "",
+      info: {
+        name: "",
+        cover: "",
+        subHead: "",
+        original: "",
+        isbn: "",
+        author: "",
+        publish: "",
+        date: "",
+        synopsis: {
+          author: "",
+          content: ""
+        }
+      }
+    };
+  },
+  methods: {
+    uploadImg: function(e) {
+      let that = this;
+      let reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onloadend = function() {
+        that.info.cover = reader.result;
+      };
+      let file = e.target.files[0];
+      let param = new FormData();
+      param.append("file", file, file.name);
+      let config = {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      };
+      that.$http.post("http://localhost/upload", param, config).then(data => {
+        console.log(data);
+      });
+    },
+    print: function() {
+      console.log(this.info);
+    },
+    sumbit: function() {
+      let that = this;
+      that.$http
+        .get("http://123")
+        .then(data => {
+          return data;
+        })
+        .catch(err => {
+          console.log(err);
+          console.log("未设定交互url");
+        });
+    }
+  },
+  computed: {}
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -10,7 +10,8 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/managment',
+//   base: process.env.BASE_URL,
   routes: [
     // {
     //   path: '/',
